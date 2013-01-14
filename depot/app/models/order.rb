2ecb21b446d00cef25eb6ac79033cd2d02c50a0c
Ryan G <rgrablick@gmail.com>
2013-01-13 23:10:11 -0500

    updated and validated entry on checkout form

--- depot/app/models/order.rb
+++ depot/app/models/order.rb
@@ -1,2 +1,5 @@
 class Order < ActiveRecord::Base
+	Payment_Types = [ "Check", "Credit card", "Purchase order" ]
+	validates :name, :address, :email, presence: true
+	validates :pay_type, inclusion: Payment_Types
 end
